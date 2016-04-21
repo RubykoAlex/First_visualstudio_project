c1fd2639b2bed8fef6f7c652f89d185b593bdc1b
VRlab <VRlab@VRLAB-PC>
2016-04-21 13:57:24 +0800

    Greetings are writen

---
+++ HelloGitHub/HelloGitHub/HelloGitHub.cpp
@@ -0,0 +1,16 @@
+// HelloGitHub.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello GitHub!!!";
+
+    return 0;
+}
+
